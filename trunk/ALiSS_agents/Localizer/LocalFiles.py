82be1524eb600c36b79bb9fbab962c2a3725c6b9
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-04-23 10:32:06 +0000

    Added custom version of Localizer, refs #23659

---
+++ trunk/ALiSS_agents/Localizer/LocalFiles.py
@@ -0,0 +1,95 @@
+# -*- coding: ISO-8859-1 -*-
+# Copyright (C) 2000-2005 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
+"""
+Localizer
+
+This module provides several localized classes, that is, classes with the
+locale attribute. Currently it only defines the classes LocalDTMLFile and
+LocalPageTemplateFile, which should be used instead of DTMLFile and
+PageTemplateFile.
+"""
+
+# Import from the Standard Library
+import os
+
+# Import from itools
+from itools import get_abspath
+from itools.gettext import domains
+
+# Import Zope modules
+from Globals import DTMLFile
+
+# Import from iHotfix
+from Products.iHotfix import DomainAware
+
+
+class LocalDTMLFile(DTMLFile):
+
+    def __init__(self, name, _prefix=None, **kw):
+        apply(LocalDTMLFile.inheritedAttribute('__init__'),
+              (self, name, _prefix), kw)
+
+        domain = get_abspath(_prefix, 'locale')
+        self.class_domain = domain
+        domains.register_domain(domain, domain)
+
+
+    def _exec(self, bound_data, args, kw):
+        # Add our gettext first
+        bound_data['gettext'] = self.gettext
+        bound_data['ugettext'] = self.gettext  # XXX backwards compatibility
+        return apply(LocalDTMLFile.inheritedAttribute('_exec'),
+                     (self, bound_data, args, kw))
+
+
+    def gettext(self, message, language=None):
+        return DomainAware.gettext(message, language, self.class_domain)
+
+
+
+# Zope Page Templates (ZPT)
+# XXX Deprecated, use the i18n namespace instead.
+try:
+    from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+except ImportError:
+    # If ZPT is not installed
+    class LocalPageTemplateFile:
+        pass
+else:
+    class LocalPageTemplateFile(PageTemplateFile):
+
+        def __init__(self, name, _prefix=None, **kw):
+            apply(LocalPageTemplateFile.inheritedAttribute('__init__'),
+                  (self, name, _prefix), kw)
+
+            domain = get_abspath(_prefix, 'locale')
+            self.class_domain = domain
+            domains.register_domain(domain, domain)
+
+
+        def _exec(self, bound_data, args, kw):
+            # Add our gettext first
+            bound_data['gettext'] = self.gettext
+            bound_data['ugettext'] = self.gettext # XXX backwards compatibility
+            return apply(LocalPageTemplateFile.inheritedAttribute('_exec'),
+                         (self, bound_data, args, kw))
+
+
+        def gettext(self, message, language=None):
+            return DomainAware.gettext(message, language, self.class_domain)
---
+++ trunk/ALiSS_agents/Localizer/README.txt
@@ -0,0 +1,75 @@
+
+Localizer is the de-facto standard to build multilingual applications with
+Zope. It helps to internationalize and localize Zope products and to build
+multilingual web sites through the Management Interface. It deals with
+both user interfaces and content.
+
+
+Requirements
+
+  - Python 2.3.5 or later
+
+  - Zope 2.7.6 or later
+
+  - itools 0.9.5 or later (download from http://www.ikaaro.org/itools)
+
+  - iHotfix 0.6.1 or later (download from http://www.ikaaro.org/localizer)
+
+
+Installation
+
+  Download Localizer. Unpack the tarball and install it in the "Products"
+  directory::
+
+    $ tar xzf Localizer-1.1.3.tar.gz
+    $ cp -r Localizer-1.1.3 <Zope instance>/Products/Localizer
+
+  Another option, case you are running Unix, is to use symbolic links::
+
+    $ ln -s Localizer-1.1.3 <Zope instance>/Products/Localizer
+
+
+Resources
+
+  - "Home site":http://www.localizer.org
+
+  - "Mailing list":http://mail.ikaaro.org/mailman/listinfo/localizer
+
+  - "Bug Tracker":http://bugs.ikaaro.org/
+
+
+Examples of sites powered by Localizer
+
+  - "European Environment Agency":http://www.eea.eu.int/
+
+  - "Bank Winter":http://www.bankwinter.com/
+
+  - "Castagnari":http://www.castagnari.com/
+
+  - "Hiru":http://www.hiru.com/
+
+  - "Hotsak":http://www.hotsak.com/
+
+  - "Ego Ibarra":http://www.egoibarra.com/
+
+  - "Udaleuskaltegiak":http://www.udaleuskaltegiak.net/
+
+
+Author and License
+
+  Copyright 2001-2006  J. David Ibáñez (jdavid@itaapy.com)
+            2001 Universitat Jaume I
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
